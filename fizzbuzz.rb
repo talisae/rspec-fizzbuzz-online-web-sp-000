f8fcaf574082d3bbe2a61147705fb1072333e228
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 18:35:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(i)
-  if i % 3 == 0
-    return "Fizz"
-  elsif i % 5 == 0
+  if i % 3 && 5 == 0
+    return "FizzBuzz"
+  elsif i % 3 || 5 == 0
     return "Buzz"
   elsif i % 3 && 5 == 0
     return "FizzBuzz"
